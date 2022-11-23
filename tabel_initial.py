7406a0d492b4d5b49eb6a0ae0c987bb98341cf89
sayeh.kargari <sayeh.kargari@camaa.ir>
2022-11-23 18:07:30 +0330

    fix:cfg

--- tabel_initial.py
+++ tabel_initial.py
@@ -1,11 +1,11 @@
 from db.database import engine
 import db.models as models
 import sqlalchemy as sa
-
+import config.cfg as cfg
 
 def check_table(engine):
     insp = sa.inspect(engine)
-    if not insp.has_table("Tablets_Information", schema="digikala"):  
+    if not insp.has_table("Tablets_Information", schema=cfg.mysql["db"]):  
         models.base.metadata.create_all(bind=engine)
         return print("Tablets_Information Table Created!")
     else: return print("Tablets_Information Table already exists!")                

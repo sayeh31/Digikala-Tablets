af0bbcede9d45adb6284830b7498451961a0e680
sayeh.kargari <sayeh.kargari@camaa.ir>
2022-11-23 18:21:46 +0330

    remove un needed file

--- services.py
+++
@@ -1,9 +0,0 @@
-import database as database
-from db.models import TabletInfo
-from typing import  List
-from sqlalchemy.orm import Session
-
-def get_all_tablets(session: Session, limit: int, offset: int) -> List[TabletInfo]:
-    return session.query(TabletInfo).offset(offset).limit(limit).all()
-
-

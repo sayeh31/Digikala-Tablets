34420a41964320e111df1a13fe4ed9b9d8366800
sayeh.kargari <sayeh.kargari@camaa.ir>
2022-11-24 10:40:17 +0330

    fix:cfg file

--- config/cfg.py
+++ config/cfg.py
@@ -3,17 +3,18 @@
 import os
 
 
-DEFAULT_HOST= "127.0.0.1",
-DEFAULT_ROOT= "root",
-DEFAULT_PASS= "123456",
-DEFAULT_DATABASE= "digikala",
+DEFAULT_HOST= "127.0.0.1"
+DEFAULT_ROOT= "root"
+DEFAULT_PASS= "123456"
+DEFAULT_DATABASE= "digikala"
 
 driver={"diver_path": None}
 
 
+
 mysql = {
     "host":  os.environ.get("HOST", DEFAULT_HOST),
-    "user":os.environ.get("ROOT", DEFAULT_ROOT) ,
+    "user":  os.environ.get("ROOT", DEFAULT_ROOT) ,
     "passwd": os.environ.get("PASS", DEFAULT_PASS),
     "db": os.environ.get("DATABASE", DEFAULT_DATABASE),
 }
